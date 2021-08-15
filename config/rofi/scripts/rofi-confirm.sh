121e8e877b3b16d3ea74e0c1fcccae9a926c6acd
Nils Carlson <nt_carlson@protonmail.com>
2021-08-15 00:09:21 -0600

    fixed type in rofi script name

--- config/rofi/scripts/rofi-confirm.sh
+++ config/rofi/scripts/rofi-confirm.sh
@@ -5,7 +5,7 @@ ICON="$1"
 CANCEL=""
 CONFIRM=""
 
-case "$(echo -e "$CANCEL\n$CONFIRM" | rofi -dmenu -theme options_menu -mesg "$ICON" )" in
+case "$(echo -e "$CANCEL\n$CONFIRM" | rofi -dmenu -theme options-menu -mesg "$ICON" )" in
     "$CANCEL")  exit 1;;
     "$CONFIRM") exit 0;;
 esac
