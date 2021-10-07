3cfc367dc4668635b053cedb39b5a93e87b4471f
Nils Carlson <nt_carlson@protonmail.com>
2021-10-07 00:15:07 -0600

    Fixed env var from being set by rofi game launcher
    Previously, the script set XDG_DATA_DIR so that rofi looked for
    *.desktop files in a different directory. This had the unwanted effect
    of propagating the env var into games launched this way which caused
    some issues with games that actually used XDG_DATA_DIR (e.g. Paradox
    games).
    
    Also fixed unwanted behavior caused by the cd inside the script where
    applications like Thunar would start in the wrong directory

--- config/rofi/scripts/rofi-wrapper.sh
+++ config/rofi/scripts/rofi-wrapper.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
-cd $(dirname $(realpath $0))
+SCRIPT_DIR="$(dirname $(realpath $0))"
 
 GAME_LAUNCHER_CACHE=$HOME/.cache/rofi-game-launcher
+APP_PATH=~/.local/share/applications/rofi-game-launcher
 
 case "$1" in
     drun)     rofi -show drun -theme drun -drun-categories Curated;;
     run)      rofi -show run -theme sidebar;;
     windows)  rofi -show window -theme sidebar;;
-    options)  ./rofi-options-menu.sh;;
+    options)  $SCRIPT_DIR/rofi-options-menu.sh;;
     games)
-        ./update-game-entries.sh -q &
+        $SCRIPT_DIR/update-game-entries.sh -q &
 
-        (
-            # Temporarily overwrite XDG_DATA_HOME so that Rofi looks for
-            # .desktop files in $GAME_LAUNCHER/applications instead of
-            # ~/.local/share/applications
-            export XDG_DATA_HOME=$GAME_LAUNCHER_CACHE
-            rofi -show drun -theme games -drun-categories SteamLibrary \
-                 -cache-dir $GAME_LAUNCHER_CACHE
-        )
+        # Temporarily link then unlink the *.desktop files to the directory
+        # where rofi looks for them to avoid having them appear when using
+        # rofi normally
+        if [ ! -e $APP_PATH ]; then
+            ln -s $GAME_LAUNCHER_CACHE/applications $APP_PATH
+        fi
+
+        rofi -show drun -theme games -drun-categories SteamLibrary \
+             -cache-dir $GAME_LAUNCHER_CACHE
+
+        if [ -h $APP_PATH ]; then
+            rm $APP_PATH
+        fi
 
         # Emulate most recently used history by resetting the count
         # to 0 for each application
