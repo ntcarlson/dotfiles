4d671f0d363ccd749086c4665c11c33a900e589e
Nils Carlson <nt_carlson@protonmail.com>
2021-12-17 01:26:41 -0700

    added vpn widget to status bar

---
+++ config/waybar/scripts/vpn.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+OPENVPN_CONF=us.protonvpn
+
+message-box() {
+    notify-send -i /usr/share/icons/Numix-Square/48/apps/qopenvpn.svg \
+                -u low \
+                -t 2000 \
+                "OpenVPN" "$1"
+}
+
+vpn-status() {
+    if vpn-is-connected; then
+        vpn-server | tr "a-z" "A-Z" | awk -F. '{print $1}'
+    else
+        echo "No VPN"
+    fi
+}
+
+vpn-connect() {
+    message-box "Starting OpenVPN"
+    sudo systemctl start openvpn-client@us.protonvpn.service
+
+    start=$(date +"%s")
+    while true; do
+        sleep 0.1
+        entry="$(journalctl /usr/bin/openvpn -r --output=short-unix | grep -m 1 -e "Initialization Sequence Completed")"
+        timestamp=$(awk -F. '{print $1}' <<< $entry)
+        now=$(date +"%s")
+        if (( timestamp + 1 > now )); then
+            message-box "Connected to \n$(vpn-server)"
+            kill -35 $(pgrep waybar)
+            break
+        fi
+        if (( now > start + 10 )); then
+            message-box "Connection timed out"
+            sudo systemctl stop openvpn-client@us.protonvpn.service
+            break
+        fi
+    done
+}
+
+vpn-disconnect() {
+    message-box "Disconnecting from \n$(vpn-server)"
+    sudo systemctl stop openvpn-client@us.protonvpn.service
+    for pid in $(pgrep -f "bash $0"); do
+        if [ ! $pid == $$ ]; then
+            kill -9 $pid
+        fi
+    done
+    kill -35 $(pgrep waybar)
+}
+
+vpn-is-connected() {
+    local status="$(systemctl show openvpn-client@us.protonvpn.service | awk -F= '/^StatusText=/ {print $2}')"
+    local active="$(systemctl show openvpn-client@us.protonvpn.service | awk -F= '/^ActiveState=/ {print $2}')"
+    if [ "$status" == "Initialization Sequence Completed" ] && [ "$active" == "active" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+vpn-server() {
+    journalctl /usr/bin/openvpn -r | grep -m 1 -Poe "([[:alnum:]]|-)*.protonvpn.com"
+}
+
+vpn-toggle() {
+    if vpn-is-connected; then
+        vpn-disconnect
+    else
+        vpn-connect
+    fi
+}
+
+case "$1" in
+    connect)    vpn-connect;;
+    disconnect) vpn-disconnect;;
+    toggle)     vpn-toggle;;
+    status)     vpn-status;;
+    *)          echo "Usage: $0 [connect|disconnect|toggle|status]"
+esac
