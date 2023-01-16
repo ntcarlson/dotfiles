9c27a4f910d9cd09a79bbe80f254938db6d242a3
Nils Carlson <nt_carlson@protonmail.com>
2023-01-16 00:48:11 -0700

    Cached icon searches for window switcher script
    Significantly improved performance of window switcher script by caching
    icon searches.

--- config/rofi/scripts/rofi-sway-window-switcher.sh
+++ config/rofi/scripts/rofi-sway-window-switcher.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Array to associate icons with appids.
+# Will fall back to a search if not specified here.
+declare -A icon_by_appid=(
+    [firefox]="firefox"
+    [kitty]="kitty"
+    [thunar]="org.xfce.thunar"
+    [org.jellyfin.jellyfinmediaplayer]="com.github.iwalton3.jellyfin-media-player"
+)
+
+# Array to associate icons with window names.
+# Will fall back to a search if not specified here.
+declare -A icon_by_name=(
+    [Mozilla Firefox]="firefox"
+    [GVIM]="gvim"
+)
 
 # Gets a list of windows in a tab separated table containing
 # Sway window id, application name, window title, and focused status 
@@ -21,29 +36,56 @@ desktop-entries() {
 # Try to match an application name or window name to a .desktop file in
 # /usr/share/applications and grab the corresponding icon
 get-icon() {
-    APPID="$1"
-    TITLE="$2"
+    # The ideal way to get application icon is from the appid
+    local appid="$1"
 
-    # First, try to guess the application based on the app_id reported by sway
-    # Sometimes, this field is empty
-    if [ -n "$APPID" ]; then
-        SEARCH=$(tr -d "-" <<< "$APPID")
-        APP=$(desktop-entries | fzf -i -f "$SEARCH" | head -n 1)
-        if [ -n "$APP" ]; then 
-            grep -m 1 -Poe '(?<=Icon=).*' $APP
+    # Some applications don't set the appid field. In these cases, we can try
+    # to guess the application assuming it follows the titling convention
+    # where the application name is added as a suffix to the title separated
+    # by a -. For example,
+    #    Getting Started - Code - OSS
+    #    [No name] - GVim
+    local title="$2"
+    local appname="${title##*- }"
+
+    # Check if the icon is already listed in one of the arrays
+    if [ -n "$appid" ] && [ -n "${icon_by_appid[$appid]}" ]; then
+        echo "${icon_by_appid[$appid]}"
+        return
+    elif [ -n "$appname" ] && [ -n "${icon_by_name[$appname]}" ]; then
+        echo "${icon_by_name[$appname]}"
+        return
+    fi
+
+    # Fallback to searching for the icon first using the appid . . .
+    if [ -n "$appid" ]; then
+        local search="$(tr -d "-" <<< "$appid")"
+        local icon="$(icon-search "$search")"
+        if [ -n "$icon" ]; then
+            icon_by_appid[$appid]="$icon"
+            echo "$icon"
             return
         fi
     fi
 
-    # If that fails, try to guess the application based on window title based
-    # on the title convention where the application name is separated by a -
-    # e.g. <page title> - Mozilla Firefox or <file name> - GVim
-    SEARCH=${TITLE##*-}
-    APP=$(desktop-entries | fzf -i -f "$SEARCH" | head -n 1)
-    if [ -n "$APP" ]; then 
-        grep -m 1 -Poe '(?<=Icon=).*' $APP
+    # . . . and then the name extracted from the title
+    if [ -n "$appname" ]; then
+        local icon="$(icon-search "$appname")"
+        if [ -n "$icon" ]; then
+            icon_by_name[$appname]="$icon"
+            echo "$icon"
+            return
+        fi
     fi
+}
 
+icon-search() {
+    local search=$1
+
+    local app="$(desktop-entries | fzf -i -f "$search" | head -n 1)"
+    if [ -n "$app" ]; then
+        grep -m 1 -Poe '(?<=Icon=).*' $app
+    fi
 }
 
 # Rofi initially calls the script with no options
