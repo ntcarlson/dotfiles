cecbecbc57bde3e44482278db65110dbfcb55601
Nils Carlson <nt_carlson@protonmail.com>
2023-01-16 01:20:35 -0700

    finished refactoring window switch script

--- config/rofi/scripts/rofi-sway-window-switcher.sh
+++ config/rofi/scripts/rofi-sway-window-switcher.sh
@@ -16,26 +16,25 @@ declare -A icon_by_name=(
     [GVIM]="gvim"
 )
 
-# Gets a list of windows in a tab separated table containing
-# Sway window id, application name, window title, and focused status 
+# Gets a list of windows in a tab separated table containing Sway window id,
+# application name, window title, and focused status. Each line of the output
+# is in a format that can be eval'd to set variables holding these values.
 get-windows() {
     swaymsg -t get_tree | \
         jq -r 'recurse(.nodes[]?, .floating_nodes[]?)
                 | select(.type=="con" or .type=="floating_con")
                 | select(.name!=null)
-                | "ID=" + (.id|tostring) 
-                + " APPID=\"" + .app_id + "\""
-                + " TITLE=\"" + .name + "\""
-                + " FOCUSED=" + (.focused|tostring)'
-}
-
-desktop-entries() {
-    find {~/.local/share/applications,/usr/share/applications} -name "*.desktop"
+                | "id=" + (.id|tostring)
+                + " appid=\"" + .app_id + "\""
+                + " title=\"" + .name + "\""
+                + " focused=" + (.focused|tostring)'
 }
 
 # Try to match an application name or window name to a .desktop file in
 # /usr/share/applications and grab the corresponding icon
 get-icon() {
+    local icon
+
     # The ideal way to get application icon is from the appid
     local appid="$1"
 
@@ -58,59 +57,75 @@ get-icon() {
     fi
 
     # Fallback to searching for the icon first using the appid . . .
-    if [ -n "$appid" ]; then
-        local search="$(tr -d "-" <<< "$appid")"
-        local icon="$(icon-search "$search")"
-        if [ -n "$icon" ]; then
-            icon_by_appid[$appid]="$icon"
-            echo "$icon"
-            return
-        fi
+    icon="$(icon-search "$(tr -d "-" <<< "$appid")")"
+    if [ -n "$icon" ]; then
+        icon_by_appid[$appid]="$icon"
+        echo "$icon"
+        return
     fi
 
-    # . . . and then the name extracted from the title
-    if [ -n "$appname" ]; then
-        local icon="$(icon-search "$appname")"
-        if [ -n "$icon" ]; then
-            icon_by_name[$appname]="$icon"
-            echo "$icon"
-            return
-        fi
+    # . . . and then using the name extracted from the title
+    icon="$(icon-search "$appname")"
+    if [ -n "$icon" ]; then
+        icon_by_name[$appname]="$icon"
+        echo "$icon"
+        return
     fi
 }
 
+# Find an application icon by trying to match a .desktop file (which will
+# specify the appropriate icon)
 icon-search() {
-    local search=$1
+    local search="$1"
+    local app
 
-    local app="$(desktop-entries | fzf -i -f "$search" | head -n 1)"
+    if [ -z "$search" ]; then
+        return
+    fi
+
+    app="$(desktop-entries | fzf -i -f "$search" | head -n 1)"
     if [ -n "$app" ]; then
-        grep -m 1 -Poe '(?<=Icon=).*' $app
+        grep -m 1 -Poe '(?<=Icon=).*' "$app"
     fi
 }
 
-# Rofi initially calls the script with no options
-if [ -z "$@" ]; then
-    i=0
+
+# Get list of desktop entries to search
+desktop-entries() {
+    find {~/.local/share/applications,/usr/share/applications} -name "*.desktop"
+}
+
+
+# Produces the list of windows for Rofi to render
+window-list() {
+    local icon
+    local i=0
     get-windows | while read -r window; do
-        # Set the ID, APPID, TITLE, and FOCUSED variables defined in the 
-        # window string
+        # Set the id, appid, title, and focused variables
         eval "$window"
 
-        ICON=$(get-icon "$APPID" "$TITLE")
-        if [ -z "$ICON" ]; then
-            echo -e "${TITLE}\0info\x1f${ID}"
+        icon=$(get-icon "$appid" "$title")
+        if [ -z "$icon" ]; then
+            echo -e "${title}\0info\x1f${id}"
         else
-            echo -e "${TITLE}\0icon\x1f${ICON}\x1finfo\x1f${ID}"
-        fi  
-        if [ "$FOCUSED" == "true" ]; then
+            echo -e "${title}\0icon\x1f${icon}\x1finfo\x1f${id}"
+        fi
+
+        if [ "$focused" == "true" ]; then
             echo -e "\0active\x1f$i"
         fi
         i=$((i + 1))
     done
+}
+
+# Rofi initially calls the script with no arguments to populate the menu.
+#
+# When an entry is selected, Rofi calls the script again with the selected
+# entry as an argument (in this case, the window title). All we care about
+# here is the ROFI_INFO variable which is set to the contents of the info
+# column of the selected entry (in this case, the sway window ID)
+if [ -z "$*" ]; then
+    window-list
 else
-    # When an entry is selected, Rofi calls the script again with the selected
-    # entry as an argument (in this case, the window title). All we care about
-    # here is the ROFI_INFO variable which is set to the contents of the info
-    # column of the selected entry (in this case, the sway window ID)
     swaymsg "[con_id=$ROFI_INFO]" focus > /dev/null
 fi
