50da0563de17d57e07c845f30414c6e07526c3fb
Nils Carlson <nt_carlson@protonmail.com>
2023-01-27 11:57:09 -0700

    Conky: Cleaned up wrapper script

--- config/conky/conky-wrapper.sh
+++ config/conky/conky-wrapper.sh
@@ -7,24 +7,23 @@ WINDOW_ID_CONKY=/tmp/conky_window_id
 conky_launch() {
     killall conky
     sleep 0.1
-    # xdotool search can't find Conky's window but fortunately Conky outputs it
-    conky 2> /tmp/conky_out
-    # Extract the hex window id from Conky's output
-    local win_id=$(awk '/drawing to created window/ {print $NF}' /tmp/conky_out | tr -d '()')
-    xdotool windowunmap $win_id
-    xdotool set_window --class "Conky" $win_id
-    echo $win_id > $WINDOW_ID_CONKY
+
+    conky &
+    local win_id="$(xdotool search --sync --class "conky")"
+    xdotool set_window --overrideredirect 1 "$win_id"
+    xdotool windowunmap "$win_id"
+    echo "$win_id" > $WINDOW_ID_CONKY
 }
 
 conky_show() {
-    local win_id=$(cat $WINDOW_ID_CONKY)
-    xdotool windowmap $win_id
-    xdotool windowraise $win_id
+    local win_id="$(cat $WINDOW_ID_CONKY)"
+    xdotool windowmap "$win_id"
+    xdotool windowraise "$win_id"
 }
 
 conky_hide() {
-    local win_id=$(cat $WINDOW_ID_CONKY)
-    xdotool windowunmap $win_id
+    local win_id="$(cat $WINDOW_ID_CONKY)"
+    xdotool windowunmap "$win_id"
 }
 
 case "$1" in
