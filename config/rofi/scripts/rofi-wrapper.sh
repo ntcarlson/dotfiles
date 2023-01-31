bb757311a37c619ed29428a8c7137e3715c79fa4
Nils Carlson <ncarlson@nmt.edu>
2023-01-31 11:09:22 -0700

    Waybar: Reverted to a static, non-expanding bar
    The method used to expand the waybar was too janky (particularly with
    multiple monitors) to be worth it. The expanding effect more or less
    still works by positioning Rofi to the left of the bar instead of the
    right.

--- config/rofi/scripts/rofi-wrapper.sh
+++ config/rofi/scripts/rofi-wrapper.sh
@@ -1,34 +1,61 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-WAYBAR="$HOME/.config/waybar/scripts/waybar-wrapper.sh"
 
 rofi-show() {
     local mode="$1"
     local theme="$2"
 
-    "$WAYBAR" resize 400
-    rofi -show "$mode" -theme "$theme" 
-    "$WAYBAR" resize 0
-}
-
-rofi-toggle() {
     local rofi_pid
-    rofi_pid="$(pgrep rofi)"
-    if [ -z "$rofi_pid" ]; then
-        $0 drun
-    else
+    pkill -SIGRTMIN+1 waybar
+    rofi_pid="$(pgrep -f "rofi -show $mode")"
+    if [ -n "$rofi_pid" ]; then
         kill -9 $rofi_pid
+    else
+        rofi_pid="$(pgrep rofi)"
+        [ -n "$rofi_pid" ] && kill -9 $rofi_pid
+        rofi -show "$mode" -theme "$theme"
     fi
+    pkill -SIGRTMIN+1 waybar
 }
 
-# Workaround to allow this script to be launched from Waybar itself without
-# terminating as soon as the SIGUSR2 signal is sent to Waybar (which tries
-# to kill all its child processes before restarting).
-trap "" TERM
+waybar-icon() {
+    local mode="$1"
+    local icon tooltip class
+    case "$mode" in
+        drun)
+            icon=""
+            tooltip="Application launcher"
+            ;;
+        run)
+            icon=""
+            tooltip="Run command"
+            ;;
+        windows)
+            icon=""
+            tooltip="Window list"
+            ;;
+        options)
+            icon=""
+            tooltip="Options menu"
+            ;;
+        *) usage;;
+    esac
+
+    rofi_pid="$(pgrep -f "$0 $mode")"
+    if [ -n "$rofi_pid" ]; then
+        class="open"
+    else
+        class="closed"
+    fi
+
+cat <<EOF
+{"text": "$icon", "tooltip": "$tooltip", "class": "$class" }
+EOF
+}
 
 usage() {
-    echo "Usage: $0 {run,drun,windows,options,toggle}"
+    echo "Usage: $0 [icon] {run,drun,windows,options}"
     exit 1
 }
 
@@ -37,6 +64,6 @@ case "$1" in
     run)     rofi-show run list;;
     windows) rofi-show window list;;
     options) $SCRIPT_DIR/rofi-options-menu.sh;;
-    toggle)  rofi-toggle;;
+    icon)    waybar-icon "$2";;
     *)       usage;;
 esac
